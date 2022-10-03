afdd1e91061f13c2de14ee29fa5f74ce3fa856cf
Thomas Kelly <84855585+1504681@users.noreply.github.com>
2022-10-03 12:56:30 -0400

    Update hacks.h

--- hacks.h
+++ hacks.h
@@ -10,6 +10,8 @@ namespace hacks {
 	struct TerrariaSignatures {
 		std::vector<int> TimeInstruction = { 0xF4, 0x00, 0x74, 0x02, 0x33, 0xF6, 0x89, 0x35, -1, -1, -1, -1 };
 		std::vector<int> LocalPlayer = { 0xA1,  -1, -1, -1, -1, 0x8B, 0x15, -1, -1, -1, -1, 0x3B, 0x50, 0x04, 0x73, 0x05, 0x8B, 0x44, 0x90, 0x08, 0xC3, 0xE8 };
+		std::vector<int> ScreenInstruction = { 0xA1, -1, -1, -1, -1, -1, -1, -1, -1, 0x83, 0xD0, -1, 0x89, 0x85, -1, -1, -1, -1, 0xDB, 0x85, -1, -1, -1, -1, 0xD9, 0x9D, -1, -1, -1, -1, 0xD9, 0x85, -1, -1, -1, -1, 0xD9,0x9D, -1, -1, -1, -1, 0xD9, 0x85, -1, -1, -1, -1, 0xD8, 0x85, -1, -1, -1, -1, 0xD8, 0x35, -1, -1, -1, -1, 0xD9, 0x9D };
+		std::vector<int> NPCArray = { 0x8B, 0x4C, -1, -1, 0x80, 0x79, 0x20, -1, 0x0F, 0x84, -1, -1, -1, -1, 0x8B, 0x91 };
 	};
 
 	struct PlayerOffsets {
@@ -17,14 +19,13 @@ namespace hacks {
 		DWORD PositionY = 0x2C;
 		DWORD VelocityX = 0x30;
 		DWORD VelocityY = 0x34;
-		DWORD NamePtr = 0x84;
-		DWORD ArmorPtr = 0xAC;
-		DWORD InventoryPtr = 0xD0;
-		DWORD BankPtr = 0xDC;
-		DWORD Health = 0x3E4;
-		DWORD MaxHealth = 0x3DC;
-
-
+		DWORD Width = 0x18;
+		DWORD Height = 0x1C;
+		DWORD NamePtr = 0x8C;
+		DWORD ArmorPtr = 0xB4;
+		DWORD InventoryPtr = 0xD8;
+		DWORD Health = 0x404;
+		DWORD MaxHealth = 0x3FC;
 	};
 
 	struct InventoryOffsets {
@@ -40,22 +41,46 @@ namespace hacks {
 	struct ItemOffsets {
 		DWORD vTable = 0x0;
 		DWORD active = 0x20;
-		DWORD type = 0x9C;
-		DWORD useAnimation = 0xA8;
-		DWORD useTime = 0xAC;
-		DWORD stack = 0xB0;
-		DWORD maxStack = 0xB4;
-		DWORD pick = 0xB8;
-		DWORD axe = 0xBC;
-		DWORD hammer = 0xC0;
-		DWORD damage = 0xD4;
-		DWORD knockback = 0xD8;
-		DWORD stringColor = 0xFC;
-		DWORD shoot = 0x108;
-		DWORD shootSpeed = 0x10C;
-		DWORD value = 0x128;
-		DWORD netID = 0x13C;
+		DWORD type = 0xA0;
+		DWORD useAnimation = 0xAC;
+		DWORD useTime = 0xB0;
+		DWORD stack = 0xB4;
+		DWORD maxStack = 0xB8;
+		DWORD pick = 0xBC;
+		DWORD axe = 0xC0;
+		DWORD hammer = 0xC4;
+		DWORD damage = 0xD8;
+		DWORD knockback = 0xDC;
+		DWORD stringColor = 0x100;
+		DWORD shoot = 0x10C;
+		DWORD shootSpeed = 0x110;
+		DWORD value = 0x12C;
+		DWORD netID = 0x140;
 
+
+	};
+
+	struct NpcOffsets {
+		DWORD width = 0x18;
+		DWORD height = 0x1C;
+		DWORD PosX = 0x38;
+		DWORD PosY = 0x3C;
+		DWORD _givenNamePtr = 0x60;
+		DWORD type = 0xE8;
+		DWORD aiStyle = 0xF0;
+		DWORD Health = 0x10C;
+		DWORD HealthMax = 0x110;
+		DWORD townNPC = 0x1b0;
+		DWORD friendly = 0x1b3;
+		
+
+
+	};
+
+	struct _givenName {
+		DWORD vTable = 0x0;
+		DWORD m_stringLength = 0x4;
+		DWORD m_firstChar = 0x8;
 
 	};
 }
