3aa942e2d2897ba1f099c0451ee98dea556ccc8c
Thomas Kelly <84855585+1504681@users.noreply.github.com>
2022-12-04 15:12:01 -0500

    Faster signature scanning w/ Boyer-Moore Algorithm

--- hacks.cpp
+++ hacks.cpp
@@ -1,41 +1,89 @@
 #include "hacks.h"
 #include <iostream>
 
-DWORD hacks::GetAddressFromSignature(std::vector<int> signature, DWORD startaddress, DWORD endaddress) {
-	SYSTEM_INFO si;
-	GetSystemInfo(&si);
-	if (startaddress == 0) {
-		startaddress = (DWORD)(si.lpMinimumApplicationAddress);
-	}
-	if (endaddress == 0) {
-		endaddress = (DWORD)(si.lpMaximumApplicationAddress);
-	}
 
-	MEMORY_BASIC_INFORMATION mbi{ 0 };
-	DWORD protectflags = (PAGE_GUARD | PAGE_NOCACHE | PAGE_NOACCESS);
+DWORD GetAddressFromSignature(std::vector<int> signature, DWORD startaddress, DWORD endaddress) {
+    // Get the system information
+    SYSTEM_INFO si;
+    GetSystemInfo(&si);
 
-	for (DWORD i = startaddress; i < endaddress - signature.size(); i++) {
-		//std::cout << "scanning: " << std::hex << i << std::endl;
-		if (VirtualQuery((LPCVOID)i, &mbi, sizeof(mbi))) {
-			if (mbi.Protect & protectflags || !(mbi.State & MEM_COMMIT)) {
-				//std::cout << "Bad Region! Region Base Address: " << mbi.BaseAddress << " | Region end address: " << std::hex << (int)((DWORD)mbi.BaseAddress + mbi.RegionSize) << std::endl;
-				i += mbi.RegionSize;
-				continue; // if bad adress then dont read from it
-			}
-			//std::cout << "Good Region! Region Base Address: " << mbi.BaseAddress << " | Region end address: " << std::hex << (int)((DWORD)mbi.BaseAddress + mbi.RegionSize) << std::endl;
-			for (DWORD k = (DWORD)mbi.BaseAddress; k < (DWORD)mbi.BaseAddress + mbi.RegionSize - signature.size(); k++) {
-				for (DWORD j = 0; j < signature.size(); j++) {
-					if (signature.at(j) != -1 && signature.at(j) != *(byte*)(k + j))
-						break;
-					if (j + 1 == signature.size())
-						return k;
-				}
-			}
-			i = (DWORD)mbi.BaseAddress + mbi.RegionSize;
-		}
-	}
-	return NULL;
+    // If no startaddress was specified, use the minimum application address
+    if (startaddress == 0) {
+        startaddress = (DWORD)(si.lpMinimumApplicationAddress);
+    }
+    // If no endaddress was specified, use the maximum application address
+    if (endaddress == 0) {
+        endaddress = (DWORD)(si.lpMaximumApplicationAddress);
+    }
+
+    // Initialize the bad character skip array
+    std::vector<int> bad_char_skip(256, signature.size());
+    for (size_t i = 0; i < signature.size(); i++) {
+        // If the character at index i is not a wildcard (-1), set the skip distance
+        // for that character to the length of the signature minus i minus 1
+        if (signature[i] != -1) {
+            bad_char_skip[signature[i]] = signature.size() - i - 1;
+        }
+    }
+
+    // Initialize the good suffix skip array
+    std::vector<int> good_suffix_skip(signature.size() + 1);
+    std::vector<int> suffixes(signature.size() + 1);
+    for (int i = signature.size(); i >= 0; i--) {
+        if (i == signature.size()) {
+            // If i is at the end of the signature, set the skip distance to 1
+            good_suffix_skip[i] = 1;
+        }
+        else {
+            // Initialize the skip distance to the length of the signature
+            good_suffix_skip[i] = signature.size();
+            for (int j = i + 1; j < signature.size(); j++) {
+                // If the character at index i - 1 is the same as the character at
+                // index j - 1 and the suffix at index j is not a wildcard (-1),
+                // set the skip distance to the length of the signature minus j
+                if (signature[i - 1] == signature[j - 1] && suffixes[j] != -1) {
+                    good_suffix_skip[i] = signature.size() - j;
+                    suffixes[i] = j;
+                    break;
+                }
+            }
+        }
+    }
+
+    // Set i to 0, which will be the starting index for the search
+    int i = 0;
+    // Continue searching while i is less than or equal to the difference between the endaddress
+    // and the startaddress minus the length of the signature
+    while (i <= (int)(endaddress - startaddress) - (int)signature.size()) {
+        // Set j to the index of the last character in the signature
+        int j = signature.size() - 1;
+        // Continue moving backwards through the signature and the memory range
+        // until a mismatch is found or j is less than 0
+        while (j >= 0 && (signature[j] == -1 || signature[j] == *(byte*)(startaddress + i + j))) {
+            j--;
+        }
+        // If j is less than 0, the signature has been found, so return the address
+        // at which it was found (startaddress + i)
+        if (j < 0) {
+            return startaddress + i;
+        }
+        // Otherwise, check whether the good suffix skip distance is greater than
+        // the bad character skip distance for the character at the current position
+        // in the memory range
+        if (good_suffix_skip[j + 1] > bad_char_skip[*(byte*)(startaddress + i + j)]) {
+            // If the good suffix skip distance is greater, use it to skip ahead
+            // in the search
+            i += good_suffix_skip[j + 1];
+        }
+        else {
+            // Otherwise, use the bad character skip distance to skip ahead in the search
+            i += bad_char_skip[*(byte*)(startaddress + i + j)];
+        }
+    }
+    // If the search is complete and the signature has not been found, return NULL
+    return NULL;
 }
+
 
 
 
@@ -43,7 +91,6 @@ DWORD hacks::GetLocalPlayer(std::vector<int> playerSig) {
 	DWORD SigStart = GetAddressFromSignature(playerSig, 0x10000000, 0x70000000);
 
 	if (SigStart != NULL) {
-		std::cout << "Player Signature Match... Calculating Address..." << std::endl;
 		DWORD eax = *(DWORD*)(*(DWORD*)(SigStart + 0x01));
 		DWORD edx = *(DWORD*)(*(DWORD*)(SigStart + 0x07));
 		return *(DWORD*)(eax + edx * 4 + 0x08);
